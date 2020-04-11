58893a3983b7070005c57c84e0785795e0817038
Manuelentrena <manuel.entrena@gmail.com>
2020-04-11 13:09:59 +0200

    Proyecto Completo

---
+++ js/mostrar_menu.js
@@ -0,0 +1,7 @@
+
+
+//Funcion click para desplegar el menu del nav en la version mobile
+function clickMenuNav() {
+    var element = document.getElementById("menu");
+    element.classList.toggle("menu_style");
+}
